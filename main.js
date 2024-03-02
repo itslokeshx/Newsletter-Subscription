fdf3f7552a50b900903be7c4c3d4db24f6e25001
LOKESH A <131357693+369loki@users.noreply.github.com>
2024-03-02 17:12:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 function userinput()
 {
 var emailName = document.getElementById("inp").value;
-If(emailName === "") 
-{alert("enter a valid email")} 
- document.getElementById("rytu").style.display="none";
- document.getElementById("top").style.display="none";
- document.getElementById("lone").innerText="Thanks for subscribing!";
- document.getElementById("ltwo").innerText="A confirmation email has been sent to -- "+ emailName+" --  Please open it and click the  button inside to confirm your subscription";
- document.getElementById("lab").style.display="none";
- document.getElementById("inp").style.display="none";
- document.getElementById("but").innerText="Dismiss message";
- document.getElementById("leftt").style.height="90%";
- document.getElementById("leftt").style.marginTop="40px";
- document.getElementById("suc").style.marginRight="81%";
- document.getElementById("ltwo").style.marginTop="8px";
- document.getElementById("ltwo").style.marginBottom="10px";
- document.getElementById("suc").style.display="flex";
+if(emailName === "") 
+{}
+else{
+  document.getElementById("rytu").style.display="none";
+  document.getElementById("top").style.display="none";
+  document.getElementById("lone").innerText="Thanks for subscribing!";
+  document.getElementById("ltwo").innerText="A confirmation email has been sent to -- "+ emailName+" --  Please open it and click the  button inside to confirm your subscription";
+  document.getElementById("lab").style.display="none";
+  document.getElementById("inp").style.display="none";
+  document.getElementById("but").innerText="Dismiss message";
+  document.getElementById("leftt").style.height="90%";
+  document.getElementById("leftt").style.marginTop="40px";
+  document.getElementById("suc").style.marginRight="81%";
+  document.getElementById("ltwo").style.marginTop="8px";
+  document.getElementById("ltwo").style.marginBottom="10px";
+  document.getElementById("suc").style.display="flex";
+} 
+
  
 }
 const mediaQuery = window.matchMedia('(max-width:600px)')
